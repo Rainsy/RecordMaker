5ca6f121d9247dc087f73d0f232cd4b7803a7f94
Jeon <Jeon@DESKTOP-HDGTHVN>
2016-05-19 20:36:38 +0900

    add deleteRecord() function unit test initRecord() when having priority in space unit test checkDuplicated()
    add debugging statement for compare case of priority

--- RecordMaker.c
+++ RecordMaker.c
@@ -8,8 +8,8 @@
 // REFER_SIZE : The number of Reference index
 // RECORD_SIZE : The range of record value (MAX - MIN + 1) regardless of index
 
-#define REFER_SIZE	2
-#define RECORD_SIZE	100000
+#define REFER_SIZE	100
+#define RECORD_SIZE	100
 
 #define UNIQUE  0
 #define DUPLI   1
@@ -27,7 +27,6 @@ struct Record {
 	// i_referSize : The number of Reference index, The value 0(zero) means that this program operates as priority in space.
 	// i_arraySize : The number of repeat count which equal to array size as twosise
 	// i_recordSize : The range of record value (MAX - MIN + 1) regardless of index
-	// i_referIndex : Current index pointer indexing p_referMap
 	// i_arrayIndex : Current index pointer indexing p_recordArr;
 	// i_maxCount : The maximum counter which one row of record will be able to store. 
 	// i_recordSize : The range of record value (MAX - MIN + 1) regardless of index
@@ -35,16 +34,23 @@ struct Record {
 	int *p_recordArr;
     int i_referSize;
 	int i_arraySize;
-	int i_referIndex;
     int i_arrayIndex;
 	int i_maxCount;
     int i_recordSize;
 };
 
+// for debug
+// total_dupliCount is that how many duplication is occured
+// total_checkReferenceCount is that how many check and calculate Reference
+int total_dupliCount;
+int total_checkReferenceCount;
+int total_checkRecordCount;
+
 int initRecord(struct Record *p_record, int _arraySize);
 int checkDuplicated(struct Record *p_record, int input);
-int addValue(struct Record *p_record);
+int addValue(struct Record *p_record, int input);
 void printRecord(const struct Record *p_record);
+void deleteRecord(struct Record *p_record);
 
 int main(int argc, char** argv)
 {
@@ -71,14 +77,15 @@ int main(int argc, char** argv)
 
         if( checkDuplicated(&r1, randRecord) == DUPLI )
         {
-             // randRecord is existed in r1 structure.
-             // Continue loop without increase of i
-             continue;
+            // randRecord is existed in r1 structure.
+            // Continue loop without increase of i
+			total_dupliCount++;
+            continue;
         }
         else
         {
             // TODO:Add record to r1 structure.
-			addValue(&r1);
+			addValue(&r1, randRecord);
             // To repeat for loop, increase i
             i++;
         }
@@ -86,6 +93,9 @@ int main(int argc, char** argv)
 
 	// Finally, Print record structure.
 	printRecord(&r1);
+
+	// Delete resource
+	deleteRecord(&r1);
 
     return 0;
 }
@@ -123,24 +133,31 @@ int initRecord(struct Record *p_record, int _arraySize)
 
 	p_record->i_recordSize = RECORD_SIZE;
 	p_record->i_referSize = REFER_SIZE;
-	p_record->i_referIndex = 0;
-	p_record->i_arraySize = _arraySize;
 	// p_recordArr has pointed twosise of arraySize
 	// ex) NTxxxxx yy
 	// First of reference is 'xxxxx' and Second is 'yy'
+	p_record->i_arraySize = _arraySize * 2;
 	p_record->p_recordArr = (int*)malloc(sizeof(int) * _arraySize * 2);
 
 	if (p_record->i_referSize == 0)
 	{
 		// priority in space
 		// none using addtional memory space
-		
+		p_record->i_referSize = 0;
 	}
 	else    // this case needs to additional memory space
 	{
-		// calculate maxCount
-		int exponent = (int)(log2(RECORD_SIZE) - log2(REFER_SIZE) + 1);
-		p_record->i_maxCount = (int)pow(2, exponent);
+		int exponent;
+	
+		exponent = log2(RECORD_SIZE) - log2(REFER_SIZE) + 1;
+
+		if (RECORD_SIZE == REFER_SIZE)
+			//in case, exponent is must be 0(zero), exception handling
+			p_record->i_maxCount = 1;
+		else
+			//in case, at least priorty space
+			//assign max Count in relation to exponent
+			p_record->i_maxCount = (int)pow(2, exponent);
 
 		// for alpha test version, exponent means row size of record.
 		// This must is larger than size of integer.
@@ -167,8 +184,85 @@ int initRecord(struct Record *p_record, int _arraySize)
  */
 int checkDuplicated(struct Record *p_record, int input)
 {
-    //TODO : realize body.
-	return UNIQUE;
+	//TODO : realize body.
+
+	int indexRecord;
+	int dupliCount;
+	int quotient;
+	int record;
+
+	if (p_record->i_referSize == 0)
+	{
+		// priority in space
+		for (indexRecord = 0;
+			indexRecord < p_record->i_arrayIndex;
+			indexRecord += 2)
+		{
+			// for debug
+			total_checkRecordCount++;
+
+			// access record array
+			record = p_record->p_recordArr[indexRecord];
+
+			// in case finding duplicated record
+			// stop finding process, retrun DUPLI
+			if (record == input)
+				return DUPLI;
+		}
+
+		// in case out of the above for loop
+		// that is regard that there is no duplicated
+		// return UNIQUE
+
+		return UNIQUE;
+
+	}
+	else
+	{
+		// for debug
+		total_checkRecordCount++;
+
+		// at least priority in time
+		quotient = input / p_record->i_maxCount;
+
+		for (indexRecord = 0, dupliCount = 0;
+			indexRecord < p_record->i_arraySize;
+			indexRecord += 2)
+		{
+			// in case finding successful as Reference count
+			// stop finding process, return UNIQUE
+			if (p_record->p_referMap[quotient] == dupliCount)
+				return UNIQUE;
+
+			// access record array
+			record = p_record->p_recordArr[indexRecord];
+
+			// in case finding duplicated record
+			// stop finding process, retrun DUPLI
+			if (record == input)
+				return DUPLI;
+
+			// in case finding value including same quotient(index)
+			// increasing dulicated count, continue loop
+			if (record / p_record->i_maxCount == quotient) {
+				total_checkReferenceCount++;
+				dupliCount++;
+			}
+		}
+
+		// in case out of the above for loop
+		// that is regard to 'p_record->p_referMap[quotient] == dupliCount'
+		// return UNIQUE
+
+		// below is just for readability
+		//if (p_record->p_referMap[quotient] == dupliCount)
+			return UNIQUE;
+	}
+
+	// in case, error
+	// be disable to access here if not debug mode
+	printf("Error, checkDuplicated(), out of access instruction\n");
+	exit(1);
 }
 
 /**
@@ -176,11 +270,12 @@ int checkDuplicated(struct Record *p_record, int input)
  @param - p_record : pointer of Record Structure.
  @return - 0 : Successful, 1 : Error.
 **/
-int addValue(struct Record *p_record)
+int addValue(struct Record *p_record, int input)
 {
 	//TODO : realize body.
 	//TODO : check whether p_arr is full or able to add
 	//TODO : returnsucessful or not
+	//TODO : increase count in which Reference.
 	int index;
 	int *p_arr;
 
@@ -193,12 +288,20 @@ int addValue(struct Record *p_record)
 		return 1;
 	}
 
-	p_arr[index] = rand() % RECORD_SIZE;
+	p_arr[index] = input;
 	p_arr[index + 1] = (rand() % 20) * 5;
 
 	p_record->i_arrayIndex += 2;
 
+	if (p_record->i_maxCount != 0) {
+		// increase count in which Reference
+		int quotient;
+		quotient = input / p_record->i_maxCount;
+
+		p_record->p_referMap[quotient] += 1;
+	}
 	return 0;
+
 }
 
 /**
@@ -210,8 +313,36 @@ void printRecord(const struct Record * p_record)
 {
 	//TODO : realize body
 
-	for (int i = 0; i < p_record->i_arraySize * 2; i+=2)
+	for (int i = 0; i < p_record->i_arraySize; i+=2)
 	{
 		printf("NT%05d %02d\n", p_record->p_recordArr[i], p_record->p_recordArr[i + 1]);
 	}
+
+	printf("\n");
+	printf("Total duplicated case : %d\n", total_dupliCount);
+	printf("Total check reference case : %d\n", total_checkReferenceCount);
+	printf("Total check record case : %d\n", total_checkRecordCount);
+}
+
+/**
+ @brief - Delete inside resource.
+ @param - p_record : pointer of Record Structure.
+ @return - none.
+*/
+void deleteRecord(struct Record * p_record)
+{
+	if (p_record->p_referMap != NULL)
+	{
+		// need to free()
+		free(p_record->p_referMap);
+		p_record->p_referMap = NULL;
+	}
+
+	if (p_record->p_recordArr != NULL)
+	{
+		// need to free()
+		free(p_record->p_recordArr);
+		p_record->p_recordArr = NULL;
+	}
+	return;
 }
